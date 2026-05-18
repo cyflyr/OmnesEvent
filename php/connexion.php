4d39c03c189ac7542791e533a597c5af91d3931f
cyprien fly <cyprien.fleury92@gmail.com>
2026-05-18 16:30:54 +0200

    modification fonctionnalité favori

--- php/connexion.php
+++ php/connexion.php
@@ -1,12 +1,6 @@
 <?php
 try {
-    // connexion à la base omnesevent (adaptée pour MAMP)
-    $bdd = new PDO(
-        'mysql:host=localhost;port=8889;dbname=omnesevent;charset=utf8mb4',
-        'root',
-        'root',
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-    );
+    $bdd = new PDO('mysql:host=localhost;dbname=omnesevent;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
     die('Erreur de connexion : ' . $e->getMessage());
 }
