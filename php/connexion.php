df90d8e2a4a33803735773ea0add990143542867
cyprien fly <cyprien.fleury92@gmail.com>
2026-05-19 19:37:20 +0200

    hebergement site web

--- php/connexion.php
+++ php/connexion.php
@@ -1,7 +1,13 @@
 <?php
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=omnesevent;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $host = 'fdb1031.your-hosting.net';
+    $nom_base = '4760740_omneseventing2g3d';
+    $utilisateur = '4760740_omneseventing2g3d';
+    $mot_de_passe = 'azerty123';
+
+    $bdd = new PDO('mysql:host=' . $host . ';port=3306;dbname=' . $nom_base . ';charset=utf8', $utilisateur, $mot_de_passe,
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
-    die('Erreur de connexion : ' . $e->getMessage());
+    die('Erreur : ' . $e->getMessage());
 }
 ?>
